59ec78e8762559694ebe88709403c9e310dc7d1c
Doug Mitchell <doug.l.mitchell@gmail.com>
2013-03-07 09:31:41 -0600

    initial commit

---
+++ DragDropExtensions.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace DRHorton.TimeKeeping.WPF.Extensions
+{
+    public static class DragDropExtensions
+    {
+        public static IObservable<EventPattern<MouseEventArgs>> ObserveMouseLeftButtonDown(this UIElement element)
+        {
+            return Observable.FromEventPattern<MouseEventArgs>(element, "PreviewMouseLeftButtonDown");
+        }
+        public static IObservable<EventPattern<MouseEventArgs>> ObserveMouseLeftButtonUp(this UIElement element)
+        {
+            return Observable.FromEventPattern<MouseEventArgs>(element, "MouseLeftButtonUp");
+        }
+        public static IObservable<EventPattern<MouseEventArgs>> ObserveMouseMove(this UIElement element)
+        {
+            return Observable.FromEventPattern<MouseEventArgs>(element, "MouseMove");
+        }
+        public static IObservable<EventPattern<MouseEventArgs>> ObserveMouseLeave(this UIElement element)
+        {
+            return Observable.FromEventPattern<MouseEventArgs>(element, "MouseLeave");
+        }
+        private static bool MinimumDragSeen(Point start, Point end)
+        {
+            return Math.Abs(end.X - start.X) >= SystemParameters.MinimumHorizontalDragDistance
+                || Math.Abs(end.Y - start.Y) >= SystemParameters.MinimumVerticalDragDistance;
+        }
+
+        public static IObservable<EventPattern<MouseEventArgs>> ObserveBeginMouseDrag(this UIElement element)
+        {
+            var mouseDown = element.ObserveMouseLeftButtonDown().Select(ep => ep.EventArgs.GetPosition(element));
+            var mouseMove = element.ObserveMouseMove();
+            
+            var stop = Observable.Merge(
+                element.ObserveMouseLeftButtonUp(),
+                element.ObserveMouseLeave().Where(ep => ep.EventArgs.LeftButton == MouseButtonState.Pressed)
+            );
+
+            return mouseDown.SelectMany(
+                md => mouseMove
+                        .Where(ep => ep.EventArgs.LeftButton == MouseButtonState.Pressed)
+                        .Where(ep => MinimumDragSeen(md, ep.EventArgs.GetPosition(element)))
+                        .TakeUntil(stop)
+                );
+
+        }
+
+        public static IObservable<EventPattern<DragEventArgs>> ObservePreviewDragEnter(this UIElement element)
+        {
+            return Observable.FromEventPattern<DragEventArgs>(element, "PreviewDragEnter");
+        }
+        public static IObservable<EventPattern<DragEventArgs>> ObservePreviewDragOver(this UIElement element)
+        {
+            return Observable.FromEventPattern<DragEventArgs>(element, "PreviewDragOver");
+        }
+        public static IObservable<EventPattern<DragEventArgs>> ObservePreviewDrop(this UIElement element)
+        {
+            return Observable.FromEventPattern<DragEventArgs>(element, "PreviewDrop");
+        }
+        public static IObservable<EventPattern<DragEventArgs>> ObservePreviewDragLeave(this UIElement element)
+        {
+            return Observable.FromEventPattern<DragEventArgs>(element, "PreviewDragLeave");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,58 @@
+RxDragAndDrop
+=============
+
+Add Drag And Drop capabilities to a xaml view by subscribing to a few observers.
+
+Observables
+-----------
+
+- ObserveMouseLeftButtonDown
+- ObserveMouseLeftButtonUp
+- ObserveMouseMove
+- ObserveMouseLeave
+- ObserveBeginMouseDrag
+- ObservePreviewDragEnter
+- ObservePreviewDragOver
+- ObservePreviewDrop
+- ObservePreivewDragLeave
+
+Usage
+-----
+
+    public partial class MyView : UserControl
+    {
+    	InitializeComponent();
+
+    	this.ObserveBeginMouseDrag()
+    		.Subscribe(e => 
+    		{
+				// collect the data to drag...
+				// var data = ...
+
+				var dragData = new DataObject("myDragData", data);
+				DragDrop.DoDragDrop(this, dragData, DragDropEffects.Move);
+    		});
+
+    	this.ObservePreviewDragOver()
+    		.Subscribe(e => 
+    		{
+    			var canDrop = e.EventArgs.Data.GetDataPresent("myDragData");
+    			e.EventArgs.Effects = canDrop ? DragDropEffects.Move : DragDropEffects.None;
+    			e.EventArgs.Handled = canDrop;
+
+    			if(canDrop)
+    				ShowDraggedAdorner(e.Sender, e.EventArgs);
+			});
+
+		this.ObservePreviewDrop()
+			.Subscribe(e => 
+			{
+				RemoveDraggedAdorner();
+				var dragData = e.EventArgs.Data.GetData("myDragData") as ...;
+
+				if(dragData != null)
+				{
+					// do something with the data
+				}
+			});
+    }
